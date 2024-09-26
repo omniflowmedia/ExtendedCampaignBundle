a9dfbf095e1d4c83c5ec4246f8ef1345a01561bf
Sunil-007 <sunilgoutham@truequations.com>
2024-09-26 13:45:15 +0530

    change

--- Assets/js/campaign-builder.js
+++ Assets/js/campaign-builder.js
@@ -1,8 +1,8 @@
 Mautic.updateTriggerIntervalUnitOptions = function () {
   const intervalType = document.getElementById('campaignevent_triggerIntervalType').value;
   const triggerStatus = mQuery('#campaignevent_triggerIntervalStatus').val();
-  const intervalValue = parseInt(document.getElementById('campaignevent_triggerInterval').value, 10);
-  
+  let intervalValue = parseInt(document.getElementById('campaignevent_triggerInterval').value, 10);
+  if(isNaN(intervalValue))intervalValue = 0;
   const intervalUnits = {
     i: {
       limits: {
